8a4a1e2044b8c8b33f8ba69d70f4b37d921625e0
Deepanshu Lanania <deepanshulavania69@gmail.com>
2025-05-04 01:21:36 +0530

    update file header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import Header from './Components/Header'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import Header from "./Components/Header";
+import PrivateRoute from "./Components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/signin' element={<SignIn/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,34 +1,60 @@
-import React from 'react'
+import React from "react";
 import { FaSearch } from "react-icons/fa";
-import {Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  
+  const { currentUser } = useSelector((state) => state.user);
+  // console.log("currentuser is ; ", currentUser.avatar);  
+
   return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-        <h1 className='font-bold text-sm sm'>
-          <span className='text-slate-500'>Deepanshu</span>
-          <span className='text-slate-700'>Estate</span>
-        </h1>  
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm">
+            <span className="text-slate-500">Deepanshu</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
         </Link>
-        <form className='bg-slate-100 p-2 rounded-lg flex gap-1 items-center md:p-3'>
-          <input type="text" placeholder='Search...' className='bg-transparent text-sm focus:outline-none w-24 sm:w-54' /> 
-          <FaSearch className='font-semibold'/>   
+        <form className="bg-slate-100 p-2 rounded-lg flex gap-1 items-center md:p-3">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent text-sm focus:outline-none w-24 sm:w-54"
+          />
+          <FaSearch className="font-semibold" />
         </form>
-        <ul className='flex gap-4'>
+        <ul className="flex gap-4">
           <NavLink to="/">
-          <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </NavLink>
           <NavLink to="/about">
-          <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>  
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </NavLink>
-          <NavLink to="/signin">
-          <li className='sm:inline text-slate-700 hover:underline'>SignIn</li>
+          <NavLink to="/profile">
+            {currentUser ? (
+              <img className="rounded-full h-8 w-8 object-conver" src={currentUser.avatar} alt="Profile" />
+            ) : (
+              <li className="sm:inline text-slate-700 hover:underline">
+                SignIn
+              </li>
+            )}
           </NavLink>
+          {/* {currentUser ?(
+            <NavLink to="/profile">
+              <img className="rounded-full h-8 w-8 object-conver" src={currentUser.avatar} alt="Profile" />
+            </NavLink>
+          ):(<NavLink to="/signin">
+            <li className="sm:inline text-slate-700 hover:underline">
+                SignIn
+              </li>
+          </NavLink>)} */}
         </ul>
       </div>
     </header>
-  )
+  );
 }
